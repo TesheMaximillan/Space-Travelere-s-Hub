5e67eeaa492295243cc0695351c1bff139f7f781
Teshome Kurabachew <dear.teshe@gmail.com>
2022-05-10 01:48:42 +0300

    Add a routing

---
+++ src/components/common/Navbar.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import logo from '../../assets/logo192.png';
+
+function Navbar() {
+  const links = [
+    {
+      id: 1,
+      path: '/rockets',
+      text: 'Rockets',
+    },
+    {
+      id: 2,
+      path: '/missions',
+      text: 'Missions',
+    },
+    {
+      id: 2,
+      path: '/myprofiles',
+      text: 'Myprofile',
+    },
+  ];
+
+  return (
+    <div className="nav">
+      <div className="nav__links">
+        <div className="logo">
+          <img src={logo} alt="Logo" />
+        </div>
+        <h1 className="logo-text">Space Traveler&apos;s Hub</h1>
+        <ul className="nav__links--link-items">
+          {links.map((link) => (
+            <li key={link.id} className="item">
+              <NavLink to={link.path} className={`item__link ${link.class}`}>{link.text}</NavLink>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
