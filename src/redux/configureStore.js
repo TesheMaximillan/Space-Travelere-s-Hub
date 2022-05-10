33605415336007969b563faa9e2cb0880b538668
Have Samuel <samiehave9@gmail.com>
2022-05-11 00:05:32 +0300

    Added a configureStore file

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -2,6 +2,12 @@ import { configurStore } from '@reduxjs/toolkit';
 import logger from 'redux-logger';
 import rocketReducer from '.rockets/reduders';
 
-const reduder = { rocketReducer };
+const reducer = { rocketReducer };
 
-const store = 
+const store = configurStore({
+  reducer,
+  middleware: (getDefaultMiddleWare) => getDefaultMiddleWare(),.concat(logger),
+  devTools: process.env.NODE_ENV != 'production',
+});
+
+export default store;
