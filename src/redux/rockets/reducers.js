3563e28528426f413d9815797c01ee22adaec1c6
Have Samuel <samiehave9@gmail.com>
2022-05-11 00:39:32 +0300

    Added the rockets dir with files that retrieve data

---
+++ src/redux/rockets/reducers.js
@@ -0,0 +1,12 @@
+import RETRIEVE_ROCKETS from './types';
+
+const rocketReducer = (state = [], action) => {
+  switch (action.type) {
+    case RETRIEVE_ROCKETS:
+      return action.rockets;
+    default:
+      return state;
+  }
+};
+
+export default rocketReducer;
