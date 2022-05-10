437a1f3b1dd487a07eac831fe53ccd56d5134915
Teshome Kurabachew <dear.teshe@gmail.com>
2022-05-10 16:31:25 +0300

    Modify Navbar styling

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -22,21 +22,31 @@ function Navbar() {
     },
   ];
 
+  const {
+    nav,
+    logoImg,
+    logoText,
+    navItems,
+    item,
+    itemLink,
+    logoContainer,
+  } = style;
+
   return (
-    <div className={style.nav}>
-      <div className={style.nav__links}>
-        <div className="logo">
-          <img src={logo} alt="Logo" />
-        </div>
-        <h1 className="logo-text">Space Traveler&apos;s Hub</h1>
-        <ul className="nav__links--link-items">
-          {links.map((link) => (
-            <li key={link.id} className="item">
-              <NavLink to={link.path} className={`item__link ${link.class}`}>{link.text}</NavLink>
-            </li>
-          ))}
-        </ul>
+    <div className={nav}>
+      <div className={logoContainer}>
+        <img src={logo} alt="Logo" className={logoImg} />
+        <h1 className={logoText}>Space Traveler&apos;s Hub</h1>
       </div>
+      <ul className={navItems}>
+        {links.map((link) => (
+          <li key={link.id} className={item}>
+            <NavLink to={link.path} className={`${itemLink}`}>
+              {link.text}
+            </NavLink>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
