77e7f60dfb74fc97ec6335261a3e51cedd3d237c
Teshome Kurabachew <dear.teshe@gmail.com>
2022-05-11 00:16:50 +0300

    Put the data in table format

--- src/routes/Missions.jsx
+++ src/routes/Missions.jsx
@@ -1,15 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
+import styles from './Missions.module.scss';
 
 function Missions() {
+  const { status, setStatus } = useState('NOT A MEMBER');
   const missions = useSelector((state) => state.missionReducer);
 
+  const { table, btnMission } = styles;
+
+  const handleStatus = () => {
+    if (status === 'NOT A MEMBER') setStatus('Active Member');
+    else setStatus('NOT A MEMBER');
+  };
+
   return (
-    <ul>
+    <table className={table}>
+      <tr>
+        <th>Mission</th>
+        <th>Description</th>
+        <th>Status</th>
+      </tr>
       {missions && missions.map((mission) => (
-        <li key={mission.mission_id}>{mission.mission_name}</li>
+        <tr key={mission.mission_id}>
+          <td>{mission.mission_name}</td>
+          <td>{mission.description}</td>
+          <td>{status}</td>
+          <td>
+            <button type="button" className={btnMission} onClick={handleStatus}>Join Mission</button>
+          </td>
+        </tr>
       ))}
-    </ul>
+    </table>
   );
 }
 
