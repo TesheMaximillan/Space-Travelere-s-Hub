0c204cb91a2e0f5a22bc7c7f5a0391e135f3f5ad
Teshome Kurabachew <dear.teshe@gmail.com>
2022-05-11 11:00:19 +0300

    Modify table structure

--- src/routes/Missions.jsx
+++ src/routes/Missions.jsx
@@ -1,35 +1,31 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useSelector } from 'react-redux';
+import Mission from '../components/Mission';
 import styles from './Missions.module.scss';
 
 function Missions() {
-  const { status, setStatus } = useState('NOT A MEMBER');
   const missions = useSelector((state) => state.missionReducer);
 
-  const { table, btnMission } = styles;
-
-  const handleStatus = () => {
-    if (status === 'NOT A MEMBER') setStatus('Active Member');
-    else setStatus('NOT A MEMBER');
-  };
+  const { table } = styles;
 
   return (
     <table className={table}>
-      <tr>
-        <th>Mission</th>
-        <th>Description</th>
-        <th>Status</th>
-      </tr>
-      {missions && missions.map((mission) => (
-        <tr key={mission.mission_id}>
-          <td>{mission.mission_name}</td>
-          <td>{mission.description}</td>
-          <td>{status}</td>
-          <td>
-            <button type="button" className={btnMission} onClick={handleStatus}>Join Mission</button>
-          </td>
+      <thead>
+        <tr>
+          <th>Mission</th>
+          <th>Description</th>
+          <th>Status</th>
+          <th> </th>
         </tr>
-      ))}
+
+      </thead>
+      <tbody>
+        {missions && missions.map((mission) => (
+          <Mission key={mission.id} mission={mission} />
+        ))}
+
+      </tbody>
+      <tfoot />
     </table>
   );
 }
