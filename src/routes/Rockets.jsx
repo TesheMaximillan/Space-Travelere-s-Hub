aa9729bed2202bd45f451c1a0872156562303fdc
Have Samuel <samiehave9@gmail.com>
2022-05-11 14:50:23 +0300

    Added a display function to display data from the API

--- src/routes/Rockets.jsx
+++ src/routes/Rockets.jsx
@@ -1,15 +1,39 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+/* eslint-disable no-param-reassign */
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
 
-function Rockets() {
-  const rockets = useSelector((state) => state.rocketReducer);
-  return (
-    <ul>
-      {rockets && rockets.map((rocket) => (
-        <li key={rocket.rocket_id}>{rocket.rocket_name}</li>
-      ))}
-    </ul>
-  );
-}
+export const getRocketData = createAsyncThunk('rocket/getData', async () =>{
+  const response = await axios('https://api.spacexdata.com/v3/rockets');
+  return response.data;
+});
 
-export default Rockets;
+const rocketSlice = createSlice({
+  name: 'rocketsSlice',
+  initialState: {
+    value: [],
+  },
+  reducers: {},
+  extraReducers: {
+    [getRocketData.fulfilled]: (state, action) => {
+      state.value = action.payload;
+    },
+  },
+});
+
+export default rocketSlice.reducer;
+
+// import React from 'react';
+// import { useSelector } from 'react-redux';
+
+// function Rockets() {
+//   const rockets = useSelector((state) => state.rocketReducer);
+//   return (
+//     <ul>
+//       {rockets && rockets.map((rocket) => (
+//         <li key={rocket.rocket_id}>{rocket.rocket_name}</li>
+//       ))}
+//     </ul>
+//   );
+// }
+
+// export default Rockets;
